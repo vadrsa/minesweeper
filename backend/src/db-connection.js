5532ce77b4f1c61b8b3dbe97d6bb258fffee8a89
Tigran <tigrancho2000@gmail.com>
2021-05-06 19:43:34 +0400

    test

--- backend/src/db-connection.js
+++ backend/src/db-connection.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_HOST, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
-
-// mongoose.set('debug', true);
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
 
 const connection = mongoose.connection;
 
-connection.once('open', function () {
-    console.log("Mongodb database connection established successfully.");
-})
+connection.once("open", function () {
+	console.log("Mongodb database  connection established successfully.");
+});
