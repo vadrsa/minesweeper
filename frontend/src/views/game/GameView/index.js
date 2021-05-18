9c5d0ce3e3ea6887a7a477a587eeca8e956c686c
Davit Asryan <Davit.Asryan@veritas.com>
2021-05-19 02:52:59 +0400

    Add stopwatch

--- frontend/src/views/game/GameView/index.js
+++ frontend/src/views/game/GameView/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
-    Box,
     Container,
     makeStyles
 } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Board from 'src/components/minesweeper/board';
+import Stopwatch from 'src/components/stopwatch';
 import api from 'src/api';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,17 +20,22 @@ const useStyles = makeStyles((theme) => ({
 const Game = () => {
     const classes = useStyles();
     const [state, setState] = useState([]);
+    const [startDate, setStartDate] = useState(Date.now());
+    const [endDate, setEndDate] = useState(0);
+    const isFinished = endDate > startDate;
 
     useEffect(() => {
         api.getGameState().then(data => {
-            setState(data[0].userArr);
+            setState(data.userArr);
+            setStartDate(Date.parse(data.startDate));
+            setEndDate(Date.parse(data.endDate));
         }).catch(e => console.log(e));
     }, [])
 
     function handleCellClick(i, j){
         api.gameClick(i, j).then(data =>{
-            console.log(data);
-            setState(data);
+            setState(data.userArr);
+            setEndDate(Date.parse(data.endDate));
         });
     }
 
@@ -40,6 +45,7 @@ const Game = () => {
             title="Game"
         >
             <Container maxWidth="lg">
+                <Stopwatch start={startDate} end={endDate}/>
                 <Board state={state} onCellClick={handleCellClick} />
             </Container>
         </Page>
